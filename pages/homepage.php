7ed0d2112789ee210666d67fa730efc6a60fff1c
SparkyGuy <tur.roberto456@gmail.com>
2023-09-30 17:54:24 -0300

    3009

--- pages/homepage.php
+++ pages/homepage.php
@@ -1,3 +1,9 @@
+<?php
+
+include('../config/protect.php');
+
+?>
+
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
@@ -10,6 +16,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <script src="../script/scripts.js" defer></script>
 </head>
 <body>
     <nav>
@@ -25,11 +32,16 @@
             </ul>
         </div>
         <div class="btn">
-
             <button href="#" class="plus-button">
+            <div id="tooltip-plus" class="hidden">
+    <span id="tooltipTextPlus">Adicionar Profissão</span>
+</div>
             <i class="fa-solid fa-plus" style="color: #6e4474; font-size: 20px;"></i>
             </button>
             <button href="#" class="profile-pic">
+            <div id="tooltip" class="hidden">
+                <span id="tooltipText"><?php echo $_SESSION['nome']; ?></span>
+            </div>
             <i class="fa-regular fa-user" style="color: #6e4474; font-size: 20px;"></i>
             </button>
         </div>
@@ -45,9 +57,75 @@
     
     <div>
         <span>$299</span>
-        <button>+</button>
+        <button id="open-modal">+</button>
     </div>
+    
+</div>
+<div class="product-card">
+    <img src="../images/pedreiro.jpg" alt="">
+    <h4>João Gomes</h4>
+    <hr>
+    <br>
+    <h5>Pedreiro</h5>
+        <h6>(16)99119-0088</h6>
+    
+    <div>
+        <span>$299</span>
+        <button id="open-modal">+</button>
+    </div>
+    
 </div>
 
-</body>
-</html>
+<script>
+    const profilePicButton = document.querySelector('.profile-pic');
+    const tooltip = document.getElementById('tooltip');
+
+    profilePicButton.addEventListener('mouseover', () => {
+        tooltip.style.display = 'block';
+    });
+
+    profilePicButton.addEventListener('mouseout', () => {
+        tooltip.style.display = 'none';
+    });
+
+    const plusButton = document.querySelector('.plus-button');
+    const tooltipPlus = document.getElementById('tooltip-plus');
+
+    plusButton.addEventListener('mouseover', () => {
+        tooltipPlus.style.display = 'block';
+    });
+
+    plusButton.addEventListener('mouseout', () => {
+        tooltipPlus.style.display = 'none';
+    });
+</script>
+
+<div id="fade" class="hide"></div>
+    <div id="modal" class="hide">
+      <div class="modal-header">
+        <h2 class="invisivel">Este é o Modal</h2>
+        <button id="close-modal">
+        <i class="fa-sharp fa-regular fa-circle-xmark fa-2xl"></i>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="cointainer">
+            <div class="elemento1">
+                <div class="profile-pic-modal">
+                    <img src="../images/pedreiro.jpg" alt="">
+                </div>
+                <h4>João Gomes</h4>
+                <h2>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                    when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                    It has survived not only five centuries, but also the leap into electronic typesetting, 
+                </h2>
+                <h3>Contato</h3>
+                <div class="links">
+                    <i class="fa-brands fa-whatsapp fa-lg"></i>
+                </div>
+            </div>
+            <div class="elemento2">
+            </div>
+      </div>
+    </div>
