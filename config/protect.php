3b8ca47171c898aec01e9acbb9a72f4bd2be1373
Arthur Roberto <93770704+SparkyGuy@users.noreply.github.com>
2023-10-23 11:23:56 -0300

    2310

--- config/protect.php
+++ config/protect.php
@@ -5,7 +5,7 @@ if(!isset($_SESSION)) {
 }
 
 if(!isset($_SESSION['nome'])) {
-    die("Você não pode acessar esta página porque não está logado.<p><a href=\"index.php\">Entrar</a></p>");
+    die("Você não pode acessar esta página porque não está logado.<p><a href=\"login.php\">Entrar</a></p>");
 }
 
 
